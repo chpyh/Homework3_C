82bcd1d8d4b0429b59438a3f75d2b0350ca3006b
chpyh <148208305+chpyh@users.noreply.github.com>
2023-11-30 05:58:59 +0300

    Task 3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,44 @@
-﻿int[] FillArray(int size)
+// ЗАДАЧА 1. Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+/*
+int[] FillArray(int size)
+{
+int[] array = new int[size];
+for (int i = 0; i < array.Length; i ++)
+{
+array[i]= new Random().Next(1, 100);
+}
+ return array;
+}
+void PrintArr(int[] numbers)
+{
+    for(int index = 0; index < numbers.Length; index ++)
+    {
+    Console.Write(numbers[index] + " ");
+    }
+    Console.WriteLine();
+}
+void FindAmmount(int[] collection, int count1, int count2)
+{
+    int ammount = 0;
+    foreach(int element in collection)
+    {
+        if(element > count1 && element < count2)
+        {
+              ammount++;
+        }
+    }
+    Console.WriteLine(ammount);
+}
+int sizeArr = 10;
+int[] arr = FillArray(sizeArr);
+int count1 = Convert.ToInt32(20);
+int count2= Convert.ToInt32(90);
+PrintArr(arr);
+FindAmmount(arr, count1, count2);
+*/
+
+//ЗАДАЧА 2. Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
+/* ﻿int[] FillArray(int size)
 {
 int[] array = new int[size];
 for (int i = 0; i < array.Length; i ++)
@@ -31,6 +71,80 @@ int sizeArr = 10;
 int[] arr = FillArray(sizeArr);
 PrintArr(arr);
 FindAmmount(arr);
+*/
+//ЗАДАЧА 3. Задайте массив из вещественных чисел с ненулевой дробной частью. Найдите разницу между максимальным и минимальным элементов массива.
+/*
+double[] FillArray(int size)
+{
+double[] array = new double[size];
+for (int position = 0; position < array.Length; position ++)
+{
+int intiger = new Random().Next();
+double fraction = new Random().NextDouble();
+array[position] = intiger + fraction;
+}
+ return array;
+}
+void PrintArr(double[] numbers)
+{
+    for(int index = 0; index < numbers.Length; index ++)
+    {
+    Console.Write(numbers[index] + " ");
+    }
+    Console.WriteLine();
+}
+double FindMax (double[] arr)
+{
+    double Max = arr[0];
+        for (int count = 0; count < arr.Length; count++)
+        {
+            if(arr[count] > Max)
+            {
+                Max = arr[count];
+            }
+        }
+         Console.WriteLine(Max);
+        return Max;
+    }
+double FindMin (double[] coll)
+{
+    double Min = coll[0];
+        for(int serialnumber = 0; serialnumber < coll.Length; serialnumber++)
+        {
+            if (coll[serialnumber] < Min)
+            {
+                Min = coll[serialnumber];
+            }
+        }
+         Console.WriteLine(Min);
+        return Min;
+    }
+void FindSubtraction(double minimum, double maximum)
+{
+   double subtraction = maximum - minimum;
+    Console.WriteLine(subtraction);
+}
+int sizeArr = 10;
+double[] collect = FillArray(sizeArr);
+PrintArr(collect);
+double maxinarr = FindMax(collect);
+double mininarr = FindMin(collect);
+FindSubtraction(mininarr,maxinarr);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
